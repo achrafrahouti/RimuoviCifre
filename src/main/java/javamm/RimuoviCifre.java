877dde20e1bdc4a00f8a18198ba17acaadf29ae2
Achraf Rahouti <arahouti@sqli.com>
2025-02-06 02:42:38 +0100

    firs exo

---
+++ src/main/java/javamm/RimuoviCifre.java
@@ -0,0 +1,21 @@
+package javamm;
+
+public class RimuoviCifre {
+
+//
+//    [REDUCED DIFFICULTY]:
+//    It can be assumed that
+//    c can only be either 1 or 2 (see ex. 1 and ex. 2).
+    public static long rimuoviCifre(long n, short c) {
+        // n = 3 2 5 5 2 2 6
+        // c=2
+
+        return 0;
+    }
+    public static void main(String[] args) {
+        short c =2;
+        long n = 3255226;
+
+
+    }
+}
